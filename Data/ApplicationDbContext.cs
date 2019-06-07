697b5f9d992db61807acd3bd77d899bfce142341
antonsher219 <36461754+antonsher219@users.noreply.github.com>
2019-06-08 01:36:24 +0300

    Add files via upload

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,5 +15,12 @@ namespace TWHelp.Data
         {
         }
         public DbSet<Topic> Topics{ get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Question> Questions{ get; set; }
+
+        public DbSet<Test> Tests{ get; set; }
+
+        public DbSet<Answer> Answers{ get; set; }
     }
 }
