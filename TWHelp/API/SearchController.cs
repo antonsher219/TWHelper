62fdbef208f276a8a45be420cb8f86a2c5c47b45
Denis Vyskrebets <d.vyskrebets@gmail.com>
2019-10-21 22:43:20 +0300

    Add UI for searching psychologists

---
+++ TWHelp/API/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TWHelp.Models.DTOs;
+
+namespace TWHelp.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        //GET: api/search/psychologist/{data}
+        [HttpGet("psychologist/{data}")]
+        public async Task<ActionResult<List<PsychologistDTO>>> Search(string data)
+        {
+            //TODO: not implemented
+            return BadRequest();
+        }
+
+    }
+}
