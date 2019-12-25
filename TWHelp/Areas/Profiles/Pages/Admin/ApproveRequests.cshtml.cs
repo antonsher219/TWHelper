26ba83de5237d566d3243c3a83fea60652e6dee1
Denis Vyskrebets <d.vyskrebets@gmail.com>
2019-12-26 00:59:21 +0200

    Fix some UI

---
+++ TWHelp/Areas/Profiles/Pages/Admin/ApproveRequests.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TWHelp.Areas.Profiles.Pages.Admin
+{
+    public class ApproveRequestsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
